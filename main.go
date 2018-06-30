d28c3e33b6826b8d786eb8b96cf3a86c25ace45a
William Ehlhardt <williamehlhardt@gmail.com>
2018-06-29 21:27:21 -0700

    multiple players; false alarms still driven by readline on server

--- main.go
+++ main.go
@@ -6,33 +6,58 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
-type Game struct {
+type PlayerName string
+
+type PlayerBoard struct {
 	falseAlarmTimes []time.Time
 }
 
-var game Game
-var falseAlarmEvents = make(chan struct{})
-var falseAlarmTimesChan = make(chan []time.Time)
+type Game struct {
+	Boards map[PlayerName]*PlayerBoard
+}
+
+var game = Game{
+	Boards: map[PlayerName]*PlayerBoard{
+		"Alice": &PlayerBoard{[]time.Time{}},
+		"Bob":   &PlayerBoard{[]time.Time{}},
+	},
+}
+var falseAlarmEvents = make(chan PlayerName)
+var gameStateChan = make(chan Game)
 
 func GameLoop() {
 	for {
 		select {
-		case <-falseAlarmEvents:
-			log.Println("got a false alarm")
-			game.falseAlarmTimes = append(game.falseAlarmTimes, time.Now())
-		case falseAlarmTimesChan <- game.falseAlarmTimes:
+		case victimName := <-falseAlarmEvents:
+			board, ok := game.Boards[victimName]
+			log.Println("got a false alarm for '", victimName, "'")
+			if !ok {
+				log.Println("...who doesn't exist")
+				continue
+			}
+			board.falseAlarmTimes = append(board.falseAlarmTimes, time.Now())
+		case gameStateChan <- game:
 			log.Println("told world about times")
 		}
 	}
 }
 
 func HandleRequest(w http.ResponseWriter, req *http.Request) {
+	log.Println("got request:", *req)
 	if req.Method == "GET" {
-		times := <-falseAlarmTimesChan
-		for _, t := range times {
+		g := <-gameStateChan
+		askerName := PlayerName(strings.TrimLeft(req.URL.Path, "/"))
+		board, ok := g.Boards[askerName]
+		if !ok {
+			log.Println("request is for player", req.URL.Path, ", who doesn't exist")
+			w.WriteHeader(404)
+			return
+		}
+		for _, t := range board.falseAlarmTimes {
 			fmt.Fprintln(w, time.Since(t))
 		}
 	}
@@ -43,11 +68,12 @@ func main() {
 	go GameLoop()
 	go (func() {
 		for {
-			_, err := bufio.NewReader(os.Stdin).ReadString('\n')
+			line, err := bufio.NewReader(os.Stdin).ReadString('\n')
 			if err != nil {
 				log.Fatal(err)
 			}
-			falseAlarmEvents <- struct{}{}
+			player := strings.TrimSpace(line)
+			falseAlarmEvents <- PlayerName(player)
 		}
 	})()
 	http.HandleFunc("/", HandleRequest)
