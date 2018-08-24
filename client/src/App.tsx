3dd6b60fe743835a4f51727a2ca33b239f3d2c8d
Spencer Pearson <speeze.pearson@gmail.com>
2018-08-23 20:00:48 -0700

    write App class

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class App extends React.Component {
+    render() {
+        return <div>&#9762; Goodbye world! &#9762;</div>;
+    }
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import { App } from './App';
+
 window.addEventListener('load', () => {
-    ReactDom.render(<div>Hello world!</div>, document.getElementById('react-root'));
-})
+    ReactDom.render(<App />, document.getElementById('react-root'));
+});
