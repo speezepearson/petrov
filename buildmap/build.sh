284fe9cc38441cb2dacc670effe5aa592843a10f
William Ehlhardt <williamehlhardt@gmail.com>
2018-09-06 22:22:57 -0700

    Map generator

---
+++ buildmap/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cat World_map_blank_without_borders.svg | sed 's/#bcbcbc/#00aaff/g' > test.svg
+inkscape     --export-png=export.png --export-dpi=100     --export-background-opacity=0 --without-gui test.svg
+convert export.png -background '#000033' -alpha remove big.png
+convert big.png -resize 200x200 smol.png
+convert smol.png -scale 1000% map.png
+optipng map.png
