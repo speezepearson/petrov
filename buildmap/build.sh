99c5c5895b4a9263ea431d883159c1ee6ee3c98a
William Ehlhardt <williamehlhardt@gmail.com>
2018-09-06 22:25:53 -0700

    Generate new map. Slightly darker. Also, remove linear-gradient because we know how to edit graphics.

--- buildmap/build.sh
+++ buildmap/build.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 
-cat World_map_blank_without_borders.svg | sed 's/#bcbcbc/#00aaff/g' > test.svg
+cat World_map_blank_without_borders.svg | sed 's/#bcbcbc/#0088dd/g' > test.svg
 inkscape     --export-png=export.png --export-dpi=100     --export-background-opacity=0 --without-gui test.svg
-convert export.png -background '#000033' -alpha remove big.png
+convert export.png -background '#000022' -alpha remove big.png
 convert big.png -resize 200x200 smol.png
 convert smol.png -scale 1000% map.png
 optipng map.png
